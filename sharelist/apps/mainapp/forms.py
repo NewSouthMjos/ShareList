8c37908fcaefa794a84fd726e241c8d8a8e58919
Evgeny <mjos1995@gmail.com>
2021-10-27 16:49:42 +0400

    in progress.. some problems with update objects

--- sharelist/apps/mainapp/forms.py
+++ sharelist/apps/mainapp/forms.py
@@ -1,6 +1,25 @@
 from django.forms import ModelForm, fields
 from mainapp.models import UserList
+from django.forms import BaseInlineFormSet
+from django.utils import timezone
+from accounts.models import CustomUser
 
+class CustomInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        # example custom validation across forms in the formset
+        for form in self.forms:
+            # your custom formset validation
+            now = timezone.now() 
+            form.cleaned_data['updated_datetime'] = now
+            form.instance.updated_datetime = now
+
+            customuser = CustomUser.objects.get(id=1)
+            form.cleaned_data['last_update_author'] = customuser
+            form.instance.last_update_author = customuser
+            print('Done cleaning data and changind datetime/author!')
+            print(form.cleaned_data)
+        
 
 class UserListForm(ModelForm):
     class Meta:
--- sharelist/apps/mainapp/views.py
+++ sharelist/apps/mainapp/views.py
@@ -1,11 +1,13 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from mainapp.models import UserList, UserListCustomUser_ReadOnly, UserListCustomUser_ReadWrite
+from mainapp.models import UserList, UserItem, UserListCustomUser_ReadOnly, UserListCustomUser_ReadWrite
 from mainapp.services.list_item_logic import get_all_userlists, get_userlist_detail
 
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from mainapp.forms import CustomInlineFormSet
 
 
 class StartPage(View):
@@ -33,4 +35,25 @@ class DetailList(LoginRequiredMixin, View):
         except LookupError:
             html = "<html><body>The list #%s was not found on server.</body></html>" % userlist_id
             return HttpResponse(html)
-        return render(request, "detailpage.html", {"form": form_userlist})
+
+        ListFormSet = inlineformset_factory(
+            UserList, UserItem, fields=('text', 'status', 'last_update_author', 'updated_datetime'), extra=1, formset=CustomInlineFormSet
+        ) 
+        userlist_obj = UserList.objects.get(id=userlist_id)
+        formset = ListFormSet(instance=userlist_obj)
+        return render(request, "detailpage.html", {"form": form_userlist, "formset": formset})
+
+    def post(self, request, userlist_id):
+        ListFormSet = inlineformset_factory(
+            UserList, UserItem, fields=('text', 'status', 'last_update_author', 'updated_datetime'), extra=1, formset=CustomInlineFormSet
+        )
+        userlist_obj = UserList.objects.get(id=userlist_id)
+        formset = ListFormSet(request.POST, instance=userlist_obj)
+        
+        
+        if formset.is_valid():
+            formset.save()
+            return redirect('/lists/')
+        else:
+            print('Form not valid')
+
