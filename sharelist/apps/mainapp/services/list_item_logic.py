7aff0090efb4c9a3cf6318688b02a3dcd1ad102a
Evgeny <mjos1995@gmail.com>
2021-11-02 14:04:10 +0400

    added exception handler, detail view taken away to services

--- sharelist/apps/mainapp/services/list_item_logic.py
+++ sharelist/apps/mainapp/services/list_item_logic.py
@@ -1,6 +1,16 @@
 from mainapp.models import UserList, UserItem, UserListCustomUser_ReadOnly, UserListCustomUser_ReadWrite
-from mainapp.forms import UserListForm
+from mainapp.forms import UserListForm, UserItemForm
 
+from django.forms.formsets import BaseFormSet
+from django.forms import formset_factory
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+
+
+def get_userlist_detail_context(user_id:int, userlist_id: int):
+    return {
+        'userlist_form': get_userlist_detail_main(user_id, userlist_id),
+        'item_formset': get_userlist_detail_items(user_id, userlist_id)
+    }
 
 def get_all_userlists(user_id: int):
     """Return all the userlists, that user can see 
@@ -19,35 +29,42 @@ def get_all_userlists(user_id: int):
     }
     return userlists
 
-def get_userlist_detail(user_id:int, userlist_id: int):
+def get_userlist_detail_main(user_id: int, userlist_id: int):
     """return the form of UserList to be displayed,
     if there is access to this list"""
     #userlist exist check
     try:
         userlist_obj = UserList.objects.get(id=userlist_id)
     except UserList.DoesNotExist:
-        raise LookupError("Userlist doesnt exist")
-    
-    #access check
+        raise ObjectDoesNotExist("The list #%s was not found on server." % userlist_id)
+    if userlist_access_check(user_id, userlist_id) < 1:
+        raise PermissionDenied("You have no access to list #%s." % userlist_id)
+    return UserListForm(initial={'title': userlist_obj.title, 'description': userlist_obj.description})
+
+def get_userlist_detail_items(user_id: int, userlist_id: int):
+    ItemFormSet = formset_factory(UserItemForm, formset=BaseFormSet)
+    items_in_requested_list = UserItem.objects.filter(related_userlist=userlist_id)
+    item_data = [{'text': i.text, 'status': i.status} for i in items_in_requested_list]
+    return ItemFormSet(initial=item_data)
+
+def userlist_access_check(user_id:int, userlist_id: int) -> int:
+    """
+    return a int stands for access rights for passed userlist_id for
+    passed user (user_id).
+    0 is no rights,
+    1 is only read,
+    2 is readwrite,
+    3 - user is author of this list (all rights)
+    """
+    userlist_obj = UserList.objects.get(id=userlist_id)
     if userlist_obj.author.id == user_id:
-        is_author = True
-    else:
-        is_author = False
-
-    if len(list(UserListCustomUser_ReadOnly.objects.filter(customuser=user_id, userlist=userlist_id))) > 0:
-        is_readonly_access = True
-    else:
-        is_readonly_access = False
-
+        return 3
     if len(list(UserListCustomUser_ReadWrite.objects.filter(customuser=user_id, userlist=userlist_id))) > 0:
-        is_readwrite_access = True
-    else:
-        is_readwrite_access = False
+        return 2
+    if len(list(UserListCustomUser_ReadOnly.objects.filter(customuser=user_id, userlist=userlist_id))) > 0:
+        return 1
+    return 0
 
-    if not(is_author or is_readonly_access or is_readwrite_access):
-        raise ValueError("No access")
-
-    return UserListForm(instance=userlist_obj)
 
     
     
--- sharelist/apps/mainapp/views.py
+++ sharelist/apps/mainapp/views.py
@@ -4,23 +4,41 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.db import IntegrityError, transaction
 from mainapp.models import UserList, UserItem, UserListCustomUser_ReadOnly, UserListCustomUser_ReadWrite
-from mainapp.services.list_item_logic import get_all_userlists, get_userlist_detail
+from mainapp.services.list_item_logic import (
+    get_all_userlists, 
+    get_userlist_detail_context
+)
 
 from django.http import HttpResponse
 from django.forms import inlineformset_factory, formset_factory
 from mainapp.forms import CustomInlineFormSet, UserListForm, UserItemForm
 from django.forms.formsets import BaseFormSet
 
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.utils import timezone
 
+class BaseView(View):
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            response = super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            html = "<html><body>%s</body></html>" % e
+            if type(e) == PermissionDenied:
+                status_code = 403
+            elif type(e) == ObjectDoesNotExist:
+                status_code = 404
+            else:
+                status_code = 500
+            return HttpResponse(html, status=status_code)
+        return response
 
 
-class StartPage(View):
+class StartPage(BaseView):
     def get(self, request):
         return render(request, "startpage.html")
 
 
-class MainPage(LoginRequiredMixin, View):
+class MainPage(LoginRequiredMixin, BaseView):
     login_url = reverse_lazy('login')
     
     def get(self, request):
@@ -29,33 +47,13 @@ class MainPage(LoginRequiredMixin, View):
 
 
 #Новая классная функция отображения.
-#Вся логика пока внутри, перенести ей в бизнес-логику
-class DetailList(LoginRequiredMixin, View):
+class DetailList(LoginRequiredMixin, BaseView):
     login_url = reverse_lazy('login')
     def get(self, request, userlist_id):
-
-        #NOT WORKING ACCESS CHECKOUT:
-        # try:
-        #     form_userlist = get_userlist_detail(request.user.id, userlist_id)
-        # except ValueError:
-        #     html = "<html><body>You have no access to list #%s.</body></html>" % userlist_id
-        #     return HttpResponse(html)
-        # except LookupError:
-        #     html = "<html><body>The list #%s was not found on server.</body></html>" % userlist_id
-        #     return HttpResponse(html)
-        user = request.user
-        ItemFormSet = formset_factory(UserItemForm, formset=BaseFormSet)
-        items_in_requested_list = UserItem.objects.filter(related_userlist=userlist_id)
-        item_data = [{'text': i.text, 'status': i.status} for i in items_in_requested_list]
-        item_formset = ItemFormSet(initial=item_data)
-        userlist_obj = UserList.objects.get(id=userlist_id)
-        userlist_form = UserListForm(initial={'title': userlist_obj.title, 'description': userlist_obj.description})
-        context = {
-            'userlist_form': userlist_form,
-            'item_formset': item_formset,
-        }
+        context = get_userlist_detail_context(request.user.id, userlist_id)
         return render(request, "detailpage.html", context)
 
+    #Вся логика пока внутри, перенести ей в бизнес-логику
     def post(self, request, userlist_id):
         userlist_form = UserListForm(request.POST)
         ItemFormSet = formset_factory(UserItemForm, formset=BaseFormSet)
