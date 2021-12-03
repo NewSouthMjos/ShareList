256280cc0e349864ed7390b6d22a41dac083da2c
Evgeny <mjos1995@gmail.com>
2021-12-03 15:35:40 +0400

    Changed login form

--- sharelist/apps/accounts/forms.py
+++ sharelist/apps/accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import (
+    UserCreationForm, UserChangeForm, AuthenticationForm
+)
+from django.contrib.auth.forms import UsernameField
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
@@ -11,3 +14,17 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+
+class CustomUserAuthenticationForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={
+        'autofocus': True,
+        'placeholder': 'Имя пользователя'
+    }))
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'current-password',
+            'placeholder': 'Пароль',
+        }),
+    )
--- sharelist/apps/accounts/views.py
+++ sharelist/apps/accounts/views.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CustomUserCreationForm
+from django.contrib.auth.views import LoginView
+from .forms import CustomUserCreationForm, CustomUserAuthenticationForm
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class LoginView(LoginView):
+    authentication_form = CustomUserAuthenticationForm
+    success_url = reverse_lazy('mainpage')
+    template_name = 'registration/login.html'
