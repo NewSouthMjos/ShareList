3afe79ff9f62e9fe6c5f52e25c1b1c90cdddb7e8
Evgeny <mjos1995@gmail.com>
2021-12-06 10:15:43 +0400

    Changed signup form

--- sharelist/apps/accounts/forms.py
+++ sharelist/apps/accounts/forms.py
@@ -1,14 +1,46 @@
 from django import forms
 from django.contrib.auth.forms import (
-    UserCreationForm, UserChangeForm, AuthenticationForm
+    UserCreationForm, UserChangeForm, AuthenticationForm, UsernameField
 )
 from django.contrib.auth.forms import UsernameField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import password_validation
+
 from .models import CustomUser
 
+class CustromUsernameField(UsernameField):
+    def widget_attrs(self, widget):
+        return {
+            **super().widget_attrs(widget),
+            'placeholder': 'Имя пользователя'
+        }
+
+
+
 class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'placeholder': 'Введите пароль'
+        }),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'placeholder': 'Повторите пароль'
+        }),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields
+        field_classes = {'username': CustromUsernameField}
         
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
