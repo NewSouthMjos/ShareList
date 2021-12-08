94d922b194f506bbee11444e8f4337c824dd58ee
Evgeny <mjos1995@gmail.com>
2021-12-08 15:06:56 +0400

    Production to HEROKU changes

--- sharelist/settings.py
+++ sharelist/settings.py
@@ -89,17 +89,18 @@ WSGI_APPLICATION = 'sharelist.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DB_password = os.getenv('DB_password')
-DATABASE_URL = os.getenv('DATABASE_URL')
-PORT = os.getenv('PORT')
+# SEE HEROKU ABOVE
+# DB_password = os.getenv('DB_password')
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# PORT = os.getenv('PORT')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER' : 'user_django',
-        'PASSWORD' : DB_password,
-        'HOST' : DATABASE_URL,
-        'PORT' : PORT,
+        'PASSWORD' : 'DB_password',
+        'HOST' : 'DATABASE_URL',
+        'PORT' : 'PORT',
     }
 }
 
@@ -196,3 +197,22 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+# heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+# Production
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
+if ENVIRONMENT == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # new
+
